eff12042b48b65a08439842e03a0f8272200d26a
Yasin Cüre <yasindenizcure@gmail.com>
2025-03-09 21:01:12 +0300

    2

---
+++ Context/CafeContext.cs
@@ -0,0 +1,17 @@
+﻿using AcunMedyaCafe.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AcunMedyaCafe.Context
+{
+    public class CafeContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-8TRT9BV\\MSSQLSERVER01; initial catalog=DbAcunMedyaCafe; integrated Security=true; TrustServerCertificate=True");
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+    }
+}
