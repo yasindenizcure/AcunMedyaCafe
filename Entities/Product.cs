20ba486743a191890c5873e36ed650b7985e17be
Yasin Cüre <yasindenizcure@gmail.com>
2025-03-09 20:40:09 +0300

    Add project files.

---
+++ Entities/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace AcunMedyaCafe.Entities
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public int ProductPrice { get; set; }
+        public string ImageUrl { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
---
+++ Entities/Testimonial.cs
@@ -0,0 +1,11 @@
+﻿namespace AcunMedyaCafe.Entities
+{
+    public class Testimonial
+    {
+        public int TestimonialId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public int Rating { get; set; }
+    }
+}
