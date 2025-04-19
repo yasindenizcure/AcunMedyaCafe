f83e51601816f42f79b57f452bc6332cd0a02cb5
Yasin Cüre <yasindenizcure@gmail.com>
2025-04-19 17:50:35 +0300

    adım1

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using AcunMedyaCafe.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AcunMedyaCafe.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly CafeContext _context;
+
+        public ProductController(CafeContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _context.Products.Include(x=>x.Category).ToList();
+            return View(values);
+        }
+    }
+}
