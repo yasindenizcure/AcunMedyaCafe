0fdfc38ab217a43737e79f4fc875289c13e5f8e2
Yasin Cüre <yasindenizcure@gmail.com>
2025-06-02 20:26:11 +0300

    Son değişiklikler.

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using AcunMedyaCafe.Context;
+
+namespace AcunMedyaCafe.Controllers
+{
+    [Authorize]
+    public class DashboardController : Controller
+    {
+        private readonly CafeContext cafeContext;
+
+        public DashboardController(CafeContext cafeContext)
+        {
+            this.cafeContext = cafeContext;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.ProductCount = cafeContext.Products.Count();
+            ViewBag.TestimonialCount = cafeContext.Testimonials.Count();
+            ViewBag.SubscriberCount = cafeContext.Subscribers.Count();
+            var subscribers = cafeContext.Subscribers.OrderByDescending(x=>x.SubscriberId).Take(5).ToList();
+            return View(subscribers);
+        }
+    }
+}
